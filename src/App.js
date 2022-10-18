d806c0fd802394487bf0c5bd52283d1576c30c1f
Yogeshwar Ghule <yghule2001@gmail.com>
2022-10-18 09:29:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Signiance Technologies NEW </h1>
+        <h1> Signiance Technologies NEW EMPIRE </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
